7c452787dbd77b9440326d4de07b84be72052cf1
NSTaft <taftnathan13@gmail.com>
2022-05-23 15:26:37 -0500

    Added computerSequence, setTimeout for playGame. Need CSS blink animation into computer sequence

---
+++ app.js
@@ -0,0 +1,42 @@
+// NEEDED 
+//RANDOM INCREMENTING COMPUTER CHOICE, STORED IN ARRAY
+// START BUTTON WITH PLAY GAME FUNCTION AS ARGUMENT
+
+// Player's sequence array
+const player = {
+    currentChoice: [],
+}
+// Computer's sequence array
+const computer = {
+    currentChoice: [],
+}
+// Determines whose turn
+let currentTurn = computer;
+
+//Call and declare the elements from HTML and CSS page
+let tiles = document.querySelectorAll(".tile");
+console.log(tiles);
+let h1 = document.querySelectorAll(".h1")
+
+
+function computerSequence(){
+    // Generate random number from tiles
+    const randomTile = Math.floor(Math.random() * tiles.length);
+    console.log(randomTile);
+    // Insert random number into sequence
+    computer.currentChoice.push(randomTile);
+    console.log(computer.currentChoice)
+    for (let tile of computer.currentChoice) {
+        //blink animation from css
+    }
+  }
+
+function playGame () {
+    computerSequence();
+
+    for (const tile of tiles) {
+        // adds listener to each box of boxes
+        tile.addEventListener("click", function(){
+            console.log(tile);
+        })}};
+setTimeout(playGame, 3000);
